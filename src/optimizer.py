a708520c9720ed4ac86a93f6ef29f96ba14546e5
valentinoli <valentin.loftsson@gmail.com>
2021-06-18 10:23:31 +0200

    Improve code and document optimizer.py

--- src/optimizer.py
+++ src/optimizer.py
@@ -1,9 +1,27 @@
+from __future__ import annotations
+
+from typing import Optional
 from .mixture import Mixture
+from .helpers import get_attrs, Z_k_1, get_gamma
 import numpy as np
 
 
-def em(model: Mixture, max_iterations, convergence_likelihood=None, convergence_threshold=1e-6) -> int:
-    """Expectation Maximization Algorithm"""
+def em(
+    model: Mixture,
+    max_iterations: int,
+    convergence_likelihood: Optional[float] = None,
+    convergence_threshold: float = 1e-6
+) -> int:
+    """
+    Expectation Maximization Algorithm
+    Args:
+        model: the mixture model
+        max_iterations: max number of iterations of the algorithm
+        convergence_likelihood: the target likelihood
+        convergence_threshold: acceptable difference between the estimate and the target likelihood
+    Returns:
+        the number of iterations required to converge, or the max number of iterations
+    """
     for i in range(max_iterations):
         model.e_step()
         likelihood_est = model.objective(model.params)
@@ -13,69 +31,76 @@ def em(model: Mixture, max_iterations, convergence_likelihood=None, convergence_
         if convergence_likelihood is not None \
                 and likelihood_est - convergence_likelihood < convergence_threshold:
             return i + 1
-
     return max_iterations
 
 
-def ula(model, nb_iters, nb_exps, error = 10**(-6), gamma = None, exp_mu = None, exp_U = None, timeout = 50000):
+
+
+def ula(
+    model: Mixture, 
+    nb_iters: int, 
+    nb_exps: int, 
+    error: float = 1e-6, 
+    gamma: float = None, 
+    exp_mu: float = None, 
+    exp_U: float = None, 
+    timeout: int = 50000
+):
     """
     If exp_mu and exp_U are left blank, the algorithm will run for nb_iters. Otherwise, it will run until
     it has fullfilled the paper's convergence criteria, or until it timeouts.
     Args:
         model: Mixture model with all the objective function paramters and gradient
-        gamma (float): step size
         nb_iters (int): number of iterations
         nb_exps: number of samples computed in parallel
+        gamma (float): step size
         exp_mu and exp_U: Expectation of the best means, and objective value of the best mean
         timeout: timeout for convergence to exp_mu and exp_U
     Returns:
         x_k: the final sample
     """
-    
-    d = model.d
-    R = model.R
-    M = model.M
-    m = model.m
-    if gamma == None:
-        gamma = 252/(d**2) - 13
+    d, R, M, m = get_attrs(model, ['d', 'R', 'M', 'm'])
+    gamma = get_gamma(gamma, d)
         
     x_k = np.zeros((nb_exps,M,d))
     for i in range(nb_exps):
         x_k[i] = model.init_params(True)
 
-    #Gaussian noise
-    def Z_k_1(): return np.random.normal(0,2*gamma,(M,d))
-    if exp_mu == None and exp_U == None:
+    if exp_mu is None and exp_U is None:
         for i in range(nb_iters - 1):
             grad_x_k = model.gradient(x_k, error)
 
-            #Samples the diffusion paths, using Euler-Maruyama scheme:
-            x_k_1 = x_k - gamma * grad_x_k 
-            +  Z_k_1()
-
+            # Samples the diffusion paths, using Euler-Maruyama scheme:
+            x_k_1 = x_k - gamma * grad_x_k + Z_k_1()
             x_k = x_k_1
     else:
         U_acc = model.objective(x_k[0])
         x_k_acc = x_k[0]
         iteration = 1
-        while np.absolute(U_acc/iteration - exp_U) >= 10**(-6) and np.linalg.norm(x_k_acc/iteration - exp_mu) >= 10**(-3):
+        while np.absolute(U_acc/iteration - exp_U) >= 1e-6 and np.linalg.norm(x_k_acc/iteration - exp_mu) >= 1e-3:
             grad_x_k = model.gradient(x_k, error)
-            #Samples the diffusion paths, using Euler-Maruyama scheme:
-            x_k_1 = x_k - gamma * grad_x_k 
-            +  Z_k_1()
+            # Samples the diffusion paths, using Euler-Maruyama scheme:
+            x_k_1 = x_k - gamma * grad_x_k + Z_k_1()
             
             x_k = x_k_1
             x_k_acc += x_k[0]
             U_acc += model.objective(x_k[0])
             iteration += 1
             if iteration > timeout:
-                print("Timeout")
+                print('Timeout')
                 break
-        print("Converged at iteration: " + str(iteration))    
+        print(f'Converged at iteration: {iteration}')    
 
     return  x_k
 
-def mala(model, nb_iters, nb_exps, error, gamma = None):
+
+def mala(
+    model,
+    nb_iters,
+    nb_exps,
+    error,
+    gamma: Optional[float] = None
+) ->:
     """
     Metropolis-Adjusted Langevin Algorithm
     Args:
@@ -86,28 +111,20 @@ def mala(model, nb_iters, nb_exps, error, gamma = None):
     Returns:
         x_k: the final sample
     """
+    d, L, R, M, m = get_attrs(model, ['d', 'L', 'R', 'M', 'm'])
+    gamma = get_gamma(gamma, d)
 
-    d = model.d
-    L = model.L
-    R = model.R
-    M = model.M
-    m = model.m
-    if gamma == None:
-        gamma = 252/(d**2) - 13
+    x_k = np.random.multivariate_normal(np.zeros(d), (1/L)*np.identity(d), (nb_exps, M))
     
-    x_k = np.random.multivariate_normal(np.zeros(d), (1/L)*np.identity(d), (nb_exps,M))
+    # grad_x_k = model.gradient(x_k, error)
     
-    #grad_x_k = model.gradient(x_k, error)
-    
-    #Gaussian noise
-    def Z_k_1(): return np.random.normal(0,2*gamma,(M,d))
-    
-    def transition_density(x,y,grad_x): return np.exp(-np.sum((y - x - gamma*grad_x)**2) / (4*gamma))
+    def transition_density(x, y, grad_x, gamma):
+        return np.exp(-np.sum((y - x - gamma*grad_x)**2) / (4*gamma))
     
     for i in range(nb_iters - 1):
         grad_x_k = model.gradient(x_k, error)
         
-        #Samples the diffusion paths, using Euler-Maruyama scheme:
+        # Samples the diffusion paths, using Euler-Maruyama scheme:
         x_k_1 = x_k - gamma * grad_x_k 
         + np.sqrt(2*gamma) * Z_k_1()
         
@@ -115,8 +132,8 @@ def mala(model, nb_iters, nb_exps, error, gamma = None):
         
         acceptance_ratio = np.zeros(x_k.shape[0])
         for i in range(x_k.shape[0]):
-            numerator = (model.objective(x_k_1[i])*transition_density(x_k_1[i],x_k[i],grad_x_k_1[i]))
-            denominator = (model.objective(x_k[i])*transition_density(x_k[i],x_k_1[i],grad_x_k[i]))
+            numerator = (model.objective(x_k_1[i]) * transition_density(x_k_1[i], x_k[i], grad_x_k_1[i], gamma))
+            denominator = (model.objective(x_k[i]) * transition_density(x_k[i], x_k_1[i], grad_x_k[i], gamma))
             acceptance_ratio[i] = numerator / denominator
         uniform_distr = np.random.rand(nb_exps)
         
